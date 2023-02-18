95a27ee963d64e70d99c673bb4d9b96abbdbc0c2
shanukajain <jain.shanuka@gmail.com>
2023-02-18 14:48:41 +0530

    hello

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,10 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: false,
     es2021: true,
   },
-  extends: 'airbnb-base',
   overrides: [
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
   rules: {
   },
 };
